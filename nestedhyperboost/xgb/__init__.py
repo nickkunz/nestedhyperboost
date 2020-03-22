4bc6e6a5d7e7bb7b060ea9c3196c08cbc52c6fb5
Nick Kunz <nick.kunz@columbia.edu>
2020-03-22 16:24:19 -0700

    Correct xgb subdirectory imports

--- nestedhyperboost/xgb/__init__.py
+++ nestedhyperboost/xgb/__init__.py
@@ -2,3 +2,5 @@
 Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Gradient Boosting
 https://github.com/nickkunz/nestedhyperboost
 """
+
+__all__ = ["xgb_ncv_classifier", "xgb_ncv_regressor", "xgb_params"]
