b8b0dbf652d447281e204dfb66281e58ebd3c66f
Nick Kunz <nick.kunz@columbia.edu>
2020-03-22 16:21:57 -0700

    Correct subdirectories

--- nestedhyperboost/__init__.py
+++ nestedhyperboost/__init__.py
@@ -3,4 +3,4 @@ Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Gradient Boosting
 https://github.com/nickkunz/nestedhyperboost
 """
 
-__all__ = ["xgb, lgb, cat"]
+__all__ = ["xgb", "lgb", "cat"]
