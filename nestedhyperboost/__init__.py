040ec5c127112c59335271ca3108a67eb8cabd2f
Nick Kunz <nick.kunz@me.com>
2020-03-22 02:28:25 -0700

    Update __init__.py

--- nestedhyperboost/__init__.py
+++ nestedhyperboost/__init__.py
@@ -2,7 +2,3 @@
 Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Gradient Boosting
 https://github.com/nickkunz/nestedhyperboost
 """
-
-from nestedhyperboost.xgboost.xgb_ncv_regressor import xgb_ncv_regressor
-from nestedhyperboost.xgboost.xgb_ncv_classifier import xgb_ncv_classifier
-from nestedhyperboost.xgboost.xgb_params import xgb_params
