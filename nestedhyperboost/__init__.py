e9c7de063081a17052aeb487d8e54389dda374fe
Nick Kunz <nick.kunz@columbia.edu>
2020-03-21 20:05:14 -0700

    Fix subdirectory imports

--- nestedhyperboost/__init__.py
+++ nestedhyperboost/__init__.py
@@ -12,3 +12,8 @@ from nestedhyperboost.ncv_optimizer import ncv_optimizer
 from nestedhyperboost.results import RegressResults
 from nestedhyperboost.results import MultiClassResults
 from nestedhyperboost.results import BinaryClassResults
+
+## xgboost
+from nestedhyperboost.xgboost.xgb_ncv_classifier import xgb_ncv_classifier
+from nestedhyperboost.xgboost.xgb_ncv_regressor import xgb_ncv_regressor
+from nestedhyperboost.xgboost.xgb_params import xgb_params
