6ab805359e2c0f715ecb99360174ef4ed4dfd79f
Nick Kunz <nick.kunz@columbia.edu>
2020-03-22 01:35:26 -0700

    Remove subdirectory __init__

--- nestedhyperboost/catboost/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Gradient Boosting
-https://github.com/nickkunz/nestedhyperboost
-"""
-
-from .cat_ncv_classifier import cat_ncv_classifier
-from .cat_ncv_regressor import cat_ncv_regressor
-from .cat_params import cat_params
--- nestedhyperboost/lightgbm/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Gradient Boosting
-https://github.com/nickkunz/nestedhyperboost
-"""
-
-from .lgb_ncv_classifier import lgb_ncv_classifier
-from .lgb_ncv_regressor import lgb_ncv_regressor
-from .lgb_params import lgb_params
