2f37478c0e8733ffaa171f50634205ac8df3bc40
Nick Kunz <nick.kunz@columbia.edu>
2020-04-03 20:58:44 -0700

    Simplify exit, upon bad input

--- nestedhyperboost/argument_quality.py
+++ nestedhyperboost/argument_quality.py
@@ -1,5 +1,4 @@
 ## load libraries
-import sys
 import pandas as pd
 
 ## create input quality checks for regressor and classifier
@@ -17,48 +16,48 @@ class ArgumentQuality():
         
         ## quality check for dataframe
         if type(self.data) is not pd.DataFrame:
-            sys.exit("must pass pandas dataframe into 'data' argument")
+            exit("must pass pandas dataframe into 'data' argument")
         
         ## quality check for missing values in dataframe
         if self.data.isnull().values.any():
-            sys.exit("dataframe cannot contain missing values")
+            exit("dataframe cannot contain missing values")
         
         ## quality check for y
         if isinstance(self.y, str) is False:
-            sys.exit("'y' must be a string")
+            exit("'y' must be a string")
         
         if self.y in self.data.columns.values is False:
-            sys.exit("'y' must be an header name (string) found in the dataframe")
+            exit("'y' must be an header name (string) found in the dataframe")
         
         ## quality check for loss 
         if isinstance(self.loss, str) is False:
-            sys.exit("'loss' must be a string")
+            exit("'loss' must be a string")
         
         ## quality check for k-fold outer argument
         if self.k_outer > len(self.data):
-            sys.exit("'k_outer' is greater than number of observations (rows)")
+            exit("'k_outer' is greater than number of observations (rows)")
         
         if self.k_outer < 2:
-            sys.exit("'k_outer' must be a positive integer greater than 1")
+            exit("'k_outer' must be a positive integer greater than 1")
         
         ## quality check for k-fold inner argument
         if self.k_inner > len(self.data):
-            sys.exit("'k_inner' is greater than number of observations (rows)")
+            exit("'k_inner' is greater than number of observations (rows)")
         
         if self.k_inner < 2:
-            sys.exit("'k_inner' must be a positive integer greater than 1")
+            exit("'k_inner' must be a positive integer greater than 1")
         
         ## quality check for number of evaluations
         if self.n_evals < 1:
-            sys.exit("'n_evals' must be a positive integer")
+            exit("'n_evals' must be a positive integer")
         
         ## quality check for random seed
         if self.seed < 1:
-            sys.exit("'seed 'must be a positive integer")
+            exit("'seed 'must be a positive integer")
         
         ## quality check for verbose
         if type(self.verbose) is not bool:
-            sys.exit("'verbose' must be boolean")
+            exit("'verbose' must be boolean")
 
 ## create input quality checks for optimizer
 class ArgumentQualityOptimizer(ArgumentQuality):
@@ -72,8 +71,8 @@ class ArgumentQualityOptimizer(ArgumentQuality):
         
         ## quality check for pred
         if self.pred_type in ["regress", "multi-class", "binary"] is False:
-            sys.exit("'pred' must be 'regress' or 'multi-class' or 'binary'")
+            exit("'pred' must be 'regress' or 'multi-class' or 'binary'")
         
         ## quality check for dataframe
         if type(self.params) is not dict:
-            sys.exit("'params' must be a dictionary")
+            exit("'params' must be a dictionary")
